8f720c3025efbbd114d0beea82be7943517389ab
HR-JA <jaimeh.robles@alumnos.udg.mx>
2021-04-21 15:00:34 -0500

    opciones pila agregadas

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 //#include<windows.h>
 using namespace std;
 int main(){
-    //pila p;
+    pila p;
     int opcion,num;
     do{
         system("clear");
-        //p.mostrar_pila();
+        p.mostrar_pila();
         cout<<"\n\n[1].ingresar numero\n[2].Eliminar \n[3].Mostrar tope\n[4].Mostrar pila\n[5].salir"<<endl;
         cin>>opcion;
         switch(opcion){
-            case 1:/*p.push();*/break;
-            case 2:/*p.pop();*/break;
-            case 3:/*p.mostrar_tope();*/break;   
-            case 4:/*p.mostrar_pila();*/break;
+            case 1:p.push();break;
+            case 2:p.pop();break;
+            case 3:p.mostrar_tope();break;   
+            case 4:p.mostrar_pila();break;
             default: cout<<"Opcion no encontrada:404 "<<endl;
         }
     }while(opcion!=5);
